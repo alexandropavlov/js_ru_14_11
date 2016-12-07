9f801a45c96601404e6e01e4edc0969a9918d632
Roman <kaboom.ua@gmail.com>
2016-12-07 22:36:42 +0200

    Update addCommentId.js

--- src/middlewares/addCommentId.js
+++ src/middlewares/addCommentId.js
@@ -1,8 +1,11 @@
 import { ADD_COMMENT } from '../constants'
 
 export default store => next => action => {
+  //лучше придумать более общий способ; через мидлвару будут проходить все экшины, их стоит делать максимально реюзабельными
 	if (action.type = ADD_COMMENT) {
 		console.log('middleware add_comment new', store.getState().comments.toArray().length);
+	    //Плохая логика для генерации id
+	    //лучше не мутировать объект
 		action.payload.comment.id = store.getState().comments.toArray().length
 	}
     //console.log('---', 'before: ', store.getState())
