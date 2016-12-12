da6c1f04ca9cb9db5e019177b9247fe855ff66d9
Alexander Pavlov <a.pavlov@designdepot.ru>
2016-12-12 15:00:04 +0300

    implement home tasks 6

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,13 +1,14 @@
 import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
-import { addComment } from '../AC/comments'
+import { addComment, loadComments } from '../AC/comments'
 import Comment from './Comment'
 import toggleOpen from '../decorators/toggleOpen'
 import NewCommentForm from './NewCommentForm'
+import Loader from './Loader'
 
 class CommentList extends Component {
     static propTypes = {
-        commentIds: PropTypes.array.isRequired,
+        //commentIds: PropTypes.array.isRequired,
         //from connect
         comments: PropTypes.array.isRequired,
         //from toggleOpen decorator
@@ -20,12 +21,11 @@ class CommentList extends Component {
     }
 
 
-    componentWillReceiveProps() {
-        //console.log('---', 'CL receiving props')
-    }
-
-    componentWillUpdate() {
-        //console.log('---', 'CL will update')
+    componentWillReceiveProps(nextProps) {
+        console.log(this.props.comments);
+        if (!this.props.isOpen && nextProps.isOpen && !this.props.comments.length) {
+            this.props.loadComments()
+        }
     }
 
 
@@ -40,20 +40,25 @@ class CommentList extends Component {
 
 
     getButton() {
-        const { comments, isOpen, toggleOpen } = this.props
-        if ( !comments.length) return <span>No comments yet</span>
+        const { article, comments, isOpen, toggleOpen } = this.props
+        if (!article.comments.length) return <span>No comments yet</span>
         return <a href="#" onClick = {toggleOpen}>{isOpen ? 'hide' : 'show'} comments</a>
     }
 
     getBody() {
-        const { article, comments, isOpen, addComment } = this.props
+        const { article, comments, isOpen, addComment, loading } = this.props
         const commentForm = <NewCommentForm articleId = {article.id} addComment = {addComment} />
-        if (!isOpen || !comments.length) return <div>{commentForm}</div>
+
+        if (!isOpen || !article.comments.length) return <div>{commentForm}</div>
+
+        if (isOpen && loading) return <div><Loader/>{commentForm}</div>
+        
         const commentItems = comments.map(comment => <li key = {comment.id}><Comment comment = {comment} /></li>)
         return <div><ul>{commentItems}</ul>{commentForm}</div>
     }
 }
 
 export default connect((state, props) => ({
-    comments: (props.article.comments || []).map(id => state.comments.get(id))
-}), { addComment })(toggleOpen(CommentList))
+    comments: state.comments.get('entities').size ? (props.article.comments || []).map(id => state.comments.getIn(['entities', id])) : [],
+    loading: state.comments.get('loading')
+}), { addComment, loadComments })(toggleOpen(CommentList))
--- src/reducer/comments.js
+++ src/reducer/comments.js
@@ -1,15 +1,32 @@
-import { ADD_COMMENT } from '../constants'
-import { normalizedComments } from '../fixtures'
-import { arrayToMap} from '../utils'
+import { ADD_COMMENT, LOAD_COMMENTS, START, SUCCESS } from '../constants'
+import { arrayToMap, ReducerState } from '../utils'
+import { Record, Map } from 'immutable'
 
-const defaultComments = arrayToMap(normalizedComments)
+const CommentModel = Record({
+    id: null,
+    user: null,
+    text: null
+})
 
-export default (comments = defaultComments, action) => {
+const defaultComments = arrayToMap([], CommentModel)
+
+const defaultState = new ReducerState({
+    entities: defaultComments,
+    loading: false
+})
+
+export default (comments = defaultState, action) => {
     const { type, payload, response, error, generatedId } = action
 
     switch (type) {
         case ADD_COMMENT:
-            return comments.set(generatedId, {...payload.comment, id: generatedId})
+            return comments.setIn(['entities', generatedId], {...payload.comment, id: generatedId})
+        case LOAD_COMMENTS + START:
+        	return comments.set('loading', true)
+        case LOAD_COMMENTS + SUCCESS:
+        	return comments
+                .set('entities', arrayToMap(response.records, CommentModel))
+                .set('loading', false)
     }
 
     return comments
